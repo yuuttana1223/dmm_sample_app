6b3b0fa7cdf63a0bcfd4167cb563257a0e258229
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-05-11 23:00:51 +0900

    [Add] 8章を追加

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -18,6 +18,16 @@ class TodolistsController < ApplicationController
     @list = List.find(params[:id])
   end
 
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
+
   private
 
   def list_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   post "todolists" => "todolists#create"
   get "todolists" => "todolists#index"
   get "todolists/:id" => "todolists#show", as: "todolist"
+  get "todolists/:id/edit" => "todolists#edit", as: "edit_todolist"
+  patch "todolists/:id" => "todolists#update", as: "update_todolist"
 end
