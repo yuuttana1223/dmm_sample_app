b2a91bb63f97677caff6a7afd49be59968fd185a
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-05-11 22:49:13 +0900

    [Add] 7章追加

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -6,7 +6,16 @@ class TodolistsController < ApplicationController
   def create
     list = List.new(list_params)
     list.save
-    redirect_to "/top"
+    # .idはあってもなくても自由
+    redirect_to todolist_path(list.id)
+  end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "top" => "homes#top"
   post "todolists" => "todolists#create"
+  get "todolists" => "todolists#index"
+  get "todolists/:id" => "todolists#show", as: "todolist"
 end
